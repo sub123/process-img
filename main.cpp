2f07087a0c7a28e461fa63a96c8cc1c241bae666
Subham <sbhm94@gmail.com>
2016-07-16 00:44:38 +0530

    cleaned code

--- main.cpp
+++ main.cpp
@@ -1,38 +1,92 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 using namespace std;
 using namespace cv;
-int main()
+void save(Mat img)
 {
-	string path;
-	cout<<"Image path:";
-	cin>>path;
-	Mat img=imread(path,CV_LOAD_IMAGE_COLOR);
-	if(!img.data)
-	{
-		cout<<"Could not load image.";
-		return -1;
-	}
-	//namedWindow("window",CV_WINDOW_AUTOSIZE);
+	cout<<"Path to save:";
+	string dest;
+	cin>>dest;
+	imwrite(dest,img);
+	waitKey(0);
+}
+void greyscale(Mat img)
+{
 	imshow("Original",img);
 	Mat gray;
 	cvtColor(img,gray,CV_BGR2GRAY);
 	imshow("Grayscale",gray);
-	//imwrite("result.jpg",img);
+	cout<<"Save image?(0/1):";
+	int schoice;
+	cin>>schoice;
+	if(schoice==1)
+	{
+		save(gray);
+	}
+	waitKey(0);
+}
+void face_detect(Mat img)
+{
+	imshow("Original",img);
+	Mat mod=img;
 	CascadeClassifier face_cascade;
 	face_cascade.load("data/haarcascades/haarcascade_frontalface_default.xml");
 	vector <Rect> faces;
-	face_cascade.detectMultiScale(img,faces,1.1,2,0|CV_HAAR_SCALE_IMAGE,Size(30,30));
+	face_cascade.detectMultiScale(mod,faces,1.1,2,0|CV_HAAR_SCALE_IMAGE,Size(30,30));
 	for(int i=0;i<=faces.size();i++)
 	{
 		Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-		ellipse( img, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+		ellipse( mod, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 	}
-	imshow("Detected Faces",img);
+	imshow("Detected Faces",mod);
+	cout<<"Save image?(0/1):";
+	int schoice;
+	cin>>schoice;
+	if(schoice==1)
+	{
+		save(mod);
+	}
 	waitKey(0);
+}
+int main()
+{
+	cout<<"1.Convert to greyscale"<<endl<<"2.Detect faces"<<endl<<"3.Use camera"<<endl<<"Enter choice:";
+	int choice;
+	cin>>choice;
+	if(choice==3)
+	{
+		cam();
+	}
+	else if(choice ==1 || choice ==2)
+	{
+		string path;
+		cout<<"Image path:";
+		cin>>path;
+		Mat img=imread(path,CV_LOAD_IMAGE_COLOR);
+		if(!img.data)
+		{
+			cout<<"Could not load image.";
+			return -1;
+		}
+		if(choice==1)
+		{
+			greyscale(img);
+		}
+		else if(choice==2)
+		{
+			face_detect(img);
+		}
+	}
+	else
+	{
+		cout<<"Invalid choice";
+		return -1;
+	}
 	return 0;
 }
